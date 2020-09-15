48eea4f2b5778ea609cfce6718e3291dda9fae26
paulsvh <pvonhagen26@gmail.com>
2020-09-14 20:58:20 -0700

    stubbed out user associations and validations in user class

--- flipnotes-backend/app/models/user.rb
+++ flipnotes-backend/app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_secure_password
+    has_many :decks
+    has_many :cards, through: :decks
+
+    validates :username, uniqueness: true, presence: true
+    validates :email uniqueness: true, presence: true
+
 end
